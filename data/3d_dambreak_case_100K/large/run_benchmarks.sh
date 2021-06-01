8ecbd9282ca207ae1dba1e9ba8f818c0e429bb82
Denis Demidov <dennis.demidov@gmail.com>
2021-06-01 21:04:12 +0300

    Update data

--- data/3d_dambreak_case_100K/large/run_benchmarks.sh
+++ data/3d_dambreak_case_100K/large/run_benchmarks.sh
@@ -3,7 +3,9 @@ pushd ../..
 source .env
 popd
 
-reuse_b4     precond.coarse_enough=500 precond.coarsening.type=aggregation precond.relax.type=damped_jacobi solver.tol=1e-6 -i 0 -s 2    2>cpu-0-part.time | tee cpu-0-part.log
-reuse_b4     precond.coarse_enough=500 precond.coarsening.type=aggregation precond.relax.type=damped_jacobi solver.tol=1e-6 -i 0 -s 2 -f 2>cpu-0-full.time | tee cpu-0-full.log
-reuse_b4_gpu precond.coarse_enough=500 precond.coarsening.type=aggregation precond.relax.type=damped_jacobi solver.tol=1e-6 -i 0 -s 2    2>gpu-0-part.time | tee gpu-0-part.log
-reuse_b4_gpu precond.coarse_enough=500 precond.coarsening.type=aggregation precond.relax.type=damped_jacobi solver.tol=1e-6 -i 0 -s 2 -f 2>gpu-0-full.time | tee gpu-0-full.log
+params="precond.coarse_enough=500 precond.coarsening.type=aggregation precond.coarsening.aggr.eps_strong=0 precond.relax.type=damped_jacobi solver.tol=1e-6"
+
+reuse_b4     $params -i 0 -s 2 -f 2>cpu-0-full.time | tee cpu-0-full.log
+reuse_b4     $params -i 0 -s 2    2>cpu-0-part.time | tee cpu-0-part.log
+reuse_b4_gpu $params -i 0 -s 2 -f 2>gpu-0-full.time | tee gpu-0-full.log
+reuse_b4_gpu $params -i 0 -s 2    2>gpu-0-part.time | tee gpu-0-part.log
