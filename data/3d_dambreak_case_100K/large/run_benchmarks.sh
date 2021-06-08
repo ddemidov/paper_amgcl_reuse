f676b14d93e558c2042d162cfec890447584d6e0
Denis Demidov <dennis.demidov@gmail.com>
2021-06-08 09:51:45 +0300

    Update large data

--- data/3d_dambreak_case_100K/large/run_benchmarks.sh
+++ data/3d_dambreak_case_100K/large/run_benchmarks.sh
@@ -3,7 +3,7 @@ pushd ../..
 source .env
 popd
 
-params="precond.coarse_enough=1000 precond.coarsening.type=aggregation precond.coarsening.aggr.eps_strong=0 precond.relax.type=damped_jacobi solver.tol=1e-6"
+params="precond.coarse_enough=500 precond.coarsening.type=aggregation precond.coarsening.aggr.eps_strong=0 precond.relax.type=damped_jacobi solver.tol=1e-6"
 
 reuse_b4     $params -i 0 -s 2 -f 2>cpu-0-none.time | tee cpu-0-none.log
 reuse_b4_gpu $params -i 0 -s 2 -f 2>gpu-0-none.time | tee gpu-0-none.log
